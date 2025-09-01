03e4ceea7b9348e25272c550dfa8e04efbc71742
Usman Naeem <naeemmusman@yahoo.com>
2025-09-01 12:08:35 +0100

    WIP

--- apps/api/src/swagger.ts
+++ apps/api/src/swagger.ts
@@ -77,8 +77,6 @@ const swaggerDefinition = {
     }
 };
 
-// const apisPath = path.resolve(__dirname + '**/*.routes.js');
-
 const options = {
     swaggerDefinition,
     apis: [path.resolve(__dirname + '**/*.routes.js')], // Path to the API docs
@@ -87,18 +85,6 @@ const options = {
 const swaggerSpec = swaggerJsDoc(options);
 
 export const swaggerUiSetup = (app: express.Application) => {
-
-    try {
-        const srcDir = path.resolve(__dirname, '../../api/src');
-        const files = fs.readdirSync(srcDir, { withFileTypes: true });
-        console.log(
-        '📂 Files in src/:',
-        files.map((f) => f.name)
-        );
-    } catch (err) {
-        console.warn('⚠️ Could not list src directory:', err.message);
-    }
-  
     const { API_HOST, API_PORT, API_PREFIX } = process.env;
     app.use(`${API_PREFIX}/documentation`, swaggerUi.serve, swaggerUi.setup(swaggerSpec));
     console.log(`API documentation available at ${API_HOST}:${API_PORT}${API_PREFIX}/documentation`);
